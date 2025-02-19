2a01e58ce339209a1e54a11725815c659c8e5594
COMLAB <rcarbungco23-0353@cca.edu.ph>
2025-02-19 18:40:40 +0800

    push mona yan

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 
 <html>
 <head>
-    <title></title>
+    <title>Javascript Form</title>
 </head>
     <body>
-        <form action="/action_page.php">
-            <fieldset>
-                <legend>Personalia:</legend>
-                <label for="fname">First name:</label>
-                <input type="text" id="fname" name="fname"><br><br>
-                <label for="lname">Last name:</label>
-                <input type="text" id="lname" name="lname"><br><br>
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email"><br><br>
-                <label for="birthday">Birthday:</label>
-                <input type="date" id="birthday" name="birthday"><br><br>
-                <input type="submit" value="Submit">
-            </fieldset>
+        <form action="action_page.php" method="GET">
+                <fieldset>
+                    <legend>Personalia:</legend>
+                    <label for="fname">First name:</label>
+                    <input type="text" id="fname" name="fname"><br><br>
+                    <label for="lname">Last name:</label>
+                    <input type="text" id="lname" name="lname"><br><br>
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email"><br><br>
+                    <label for="birthday">Birthday:</label>
+                    <input type="date" id="birthday" name="birthday"><br><br>
+                    <input type="submit" value="SAVE RECORD" name="submit"/>
+                </fieldset>
         </form>
     </body>
 </html>
